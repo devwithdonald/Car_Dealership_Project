d5df33fea10d07567352ac17e3f82417a7098339
devwithdonald <hend.donald@gmail.com>
2019-06-05 22:34:15 -0400

    sign up functionality for customer is complete. only accepts unique usernames

--- src/com/donald/screens/CustomerScreen.java
+++ src/com/donald/screens/CustomerScreen.java
@@ -5,62 +5,60 @@ import java.util.Scanner;
 import com.donald.users.Employee;
 import com.donald.users.MasterCustomerLoginList;
 
-public class CustomerScreen implements UserScreen{
+public class CustomerScreen implements UserScreen {
 
 	@Override
 	public boolean display() {
 		// TODO Auto-generated method stub
-		
-		//ask if they need to sign up or login
-		if(!customerAccess()) {
+
+		// ask if they need to sign up or login
+		if (!customerAccess()) {
 			return false;
 		}
-		
+
 //		if(!loginVerification()) {
 //			return false;
 //		}
-		
-		//TODO here 
-		
+
+		// TODO here
+
 		return false;
 	}
 
 	@Override
 	public boolean loginVerification() {
 		System.out.println("-- Login Verification -- ");
-		
+
 		int counter = 0;
 		String username = "";
 		boolean verifiedUsername = false;
 		String password = "";
 		boolean verifiedPassword = true;
 		boolean exitInput = false;
-		
+
 		do {
 			Scanner scanner = new Scanner(System.in);
 			counter = 0;
-			
+
 			// username validation
 			System.out.println("Enter (b) to return to the initial screen.");
-			
-			
+
 			System.out.println("enter username --> ");
 			username = scanner.nextLine();
 
 			if (username.equals("b")) {
 				break;
 			}
-			
-			if(MasterCustomerLoginList.getCustomerloginmap().containsKey(username)) {
+
+			if (MasterCustomerLoginList.getCustomerloginmap().containsKey(username)) {
 				verifiedUsername = true;
 				counter++;
 			}
-			
-			
+
 			// password validation
 			System.out.println("enter password --> ");
 			password = scanner.nextLine();
-			
+
 			if (counter == 1) {
 				if (MasterCustomerLoginList.getCustomerloginmap().get(username).equals(password)) {
 					verifiedPassword = true;
@@ -70,17 +68,15 @@ public class CustomerScreen implements UserScreen{
 				System.out.println("username not found!");
 			}
 
-			//after failed password attempt
-			if(counter == 1) {
+			// after failed password attempt
+			if (counter == 1) {
 				System.out.println("wrong password");
 			}
-			
-			
-		}while (!verifiedUsername && !verifiedPassword);
-		
-		
-		//will need to ensure the username password have a match
-		//best is probably a master list with MAP
+
+		} while (!verifiedUsername && !verifiedPassword);
+
+		// will need to ensure the username password have a match
+		// best is probably a master list with MAP
 		// if not register to the map then ask again!
 		if (counter == 2) {
 			// verified let them in!
@@ -90,67 +86,71 @@ public class CustomerScreen implements UserScreen{
 			return false;
 		}
 	}
-	
-	//do loop
+
+	// do loop
 	public boolean register() {
 		System.out.println("-- Customer Registration -- ");
-		
-	
+
 		String username = "";
 		String password = "";
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("enter new username-->");
-		username = scanner.nextLine();
-		
-		System.out.println("enter new password-->");
-		password = scanner.nextLine();
-		
-		//TODO will need to check if user name is already here
-		//add to master customer login map 
-		MasterCustomerLoginList.getCustomerloginmap().put(username, password);
-		
-		
-		System.out.println("Success! '" + username + "' is now a registered user!");
-		
+		boolean exitInput = false;
+
+		do {
+
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("enter new username-->");
+			username = scanner.nextLine();
+
+			if (MasterCustomerLoginList.getCustomerloginmap().containsKey(username)) {
+				System.out.println("this username is already taken");
+
+			} else {
+				System.out.println("enter new password-->");
+				password = scanner.nextLine();
+
+				MasterCustomerLoginList.getCustomerloginmap().put(username, password);
+				System.out.println("Success! '" + username + "' is now a registered user!");
+				exitInput = true;
+			}
+
+		} while (!exitInput);
+
 		return true;
 	}
 
 	public boolean customerAccess() {
-		
+
 		boolean exitInput = false;
 		boolean returnBool = true;
 		String input = "";
-		
+
 		do {
 			Scanner scanner = new Scanner(System.in);
-			
+
 			System.out.println("Enter '1': Login as existing user");
 			System.out.println("Enter '2': Register as new user");
 			System.out.println("Enter '0': Exit!");
-			
+
 			input = scanner.nextLine();
-			
-			if(input.equals("1")) {
-				//call login
+
+			if (input.equals("1")) {
+				// call login
 				exitInput = loginVerification();
 			} else if (input.equals("2")) {
-				//call register
+				// call register
 				register();
-			} else if (input.equals("0")){
+			} else if (input.equals("0")) {
 				exitInput = true;
 				returnBool = false;
-				
+
 			}
-			
-			
-		}while(!exitInput);
-		
+
+		} while (!exitInput);
+
 		return returnBool;
-		//call register or loginVerification if not exit
-		
+		// call register or loginVerification if not exit
+
 	}
-	
-	
+
 }
