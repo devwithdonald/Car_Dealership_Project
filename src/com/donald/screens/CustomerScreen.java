a05c155f47e18353e0321e149500de3a152d91bd
devwithdonald <hend.donald@gmail.com>
2019-06-09 02:09:45 -0400

    logging progess and method clean up -> on customer screen

--- src/com/donald/screens/CustomerScreen.java
+++ src/com/donald/screens/CustomerScreen.java
@@ -16,28 +16,18 @@ import com.donald.users.CustomerBase;
 import com.donald.users.Employee;
 import com.donald.users.MasterCustomerLoginList;
 import com.donald.users.MasterOfferList;
+import com.donald.util.LoggingUtil;
 
 public class CustomerScreen implements UserScreen {
-	
-	
-	Customer loggedInCustomer;
-	//SAVE FILE
-	/*
-	 * LoginListSerializeDAO loginListData = new LoginListSerializeDAO();
-	 * CustomerListSerializeDAO customerListdata = new CustomerListSerializeDAO();
-	 * OfferSerializeDAO offerListData = new OfferSerializeDAO(); CarLotSerializeDAO
-	 * carLotData = new CarLotSerializeDAO();
-	 */
 
+	Customer loggedInCustomer;
+
+	//TODO JUNIT TEST
 	@Override
 	public boolean display() {
-		// TODO Auto-generated method stub
+		LoggingUtil.trace("CustomerScreen - display() - start");
 
-
-		
-
-		
-		if (!customerAccess()) {
+		if (!customerAccessMenu()) {
 			return false;
 		}
 
@@ -45,79 +35,45 @@ public class CustomerScreen implements UserScreen {
 		String input = "";
 
 		do {
-			//might need to load here?
-			
-			//load
-			/*
-			 * MasterOfferList.getOfferlist().clear();
-			 * 
-			 * if (offerListData.loadOfferList() != null) {
-			 * MasterOfferList.getOfferlist().addAll(offerListData.loadOfferList()); }
-			 */
-			
-			/*
-			 * //load CustomerBase.getCustomerlist().clear();
-			 * 
-			 * if (customerListdata.loadCustomerList() != null) {
-			 * CustomerBase.getCustomerlist().addAll(customerListdata.loadCustomerList()); }
-			 * 
-			 * //load CarLot.getCarlot().clear();
-			 * 
-			 * if (carLotData.loadCarLot() != null) {
-			 * CarLot.getCarlot().addAll(carLotData.loadCarLot()); }
-			 */
-			
-			
-			
-			
-			
-			
-			
-			
-			System.out.println("id-> " + loggedInCustomer.getCustomerID() + " balance-> "  + loggedInCustomer.getBalance() + " pending offers-> "
-					+ loggedInCustomer.getPendingOffers() + " monthly payment " + loggedInCustomer.getMonthlyPayment());
-			
 			Scanner scanner = new Scanner(System.in);
+			
 			System.out.println("\nWelcome " + loggedInCustomer.getUsername() + "! What would you like to do today?");
-			
-			
 			System.out.println("Enter '1': View all cars on the lot");
 			System.out.println("Enter '2': To make an offer on a car");
 			System.out.println("Enter '3': View cars that you own");
 			System.out.println("Enter '4': View remaining payments on an owned car");
 			System.out.println("Enter '0': Exit!");
-
 			input = scanner.nextLine();
 
 			CarLotServiceImpl clsi = new CarLotServiceImpl();
 			CustomerServiceImpl csi = new CustomerServiceImpl();
+
 			
-			
-			
-			
-			// call stuff
 			switch (input) {
 			case "1":
+				LoggingUtil.trace("CustomerScreen - display() - calling viewCarLot();");
 				clsi.viewCarLot();
 				exitInput = false;
 				break;
 			case "2":
-				//TODO NOT COMPLETE
+				LoggingUtil.trace("CustomerScreen - display() - calling makeOffer(loggedInCustomer);");
 				csi.makeOffer(loggedInCustomer);
 				exitInput = false;
 				break;
 			case "3":
+				LoggingUtil.trace("CustomerScreen - display() - calling viewOwnedCars(loggedInCustomer);");
 				csi.viewOwnedCars(loggedInCustomer);
 				exitInput = false;
 				break;
 			case "4":
-				//TODO
+				LoggingUtil.trace("CustomerScreen - display() - calling viewRemainingPayments(loggedInCustomer);");
 				csi.viewRemainingPayments(loggedInCustomer);
 				exitInput = false;
 				break;
 			case "0":
+				LoggingUtil.trace("CustomerScreen - display() - exiting");
 				exitInput = true;
-				System.out.println("Thank you, have a good day!\n");
+				System.out.println("Thank You, Have a Good Day!\n");
 				break;
 			}
 		} while (!exitInput);
@@ -125,68 +81,38 @@ public class CustomerScreen implements UserScreen {
 		return false;
 	}
 
+	// TODO JUNIT TEST
 	@Override
 	public boolean loginVerification() {
+		LoggingUtil.trace("CustomerScreen - loginVerification - start");
 		System.out.println("-- Login Verification -- ");
-		
-		
-//		//TODO??
-//		//CustomerBase.getCustomerlist().clear();
-//		//CustomerBase
-//		CustomerBase.getCustomerlist().addAll(customerListdata.loadCustomerList());
-//		
-//		//MasterCustomerLoginList.getCustomerloginmap().keySet().removeAll()
-//		MasterCustomerLoginList.getCustomerloginmap().putAll(loginListData.loadLoginList());
-//		
-		//TODO gives exception when list is empty but tried to load list
-		
 
-		
-	//	if(CustomerBase.getCustomerlist().size() != 0) {
-//		CustomerBase.getCustomerlist().clear();
-		
-		/*
-		 * if(customerListdata.loadCustomerList() != null &&
-		 * loginListData.loadLoginList() != null) {
-		 * CustomerBase.getCustomerlist().clear();
-		 * CustomerBase.getCustomerlist().addAll(customerListdata.loadCustomerList());
-		 * MasterCustomerLoginList.getCustomerloginmap().putAll(loginListData.
-		 * loadLoginList()); }
-		 */
-		
-		//MasterCustomerLoginList.getCustomerloginmap().keySet().removeAll()
-		
-		
-		//}
-		
-		
-		
 		int counter = 0;
+
 		String username = "";
 		boolean verifiedUsername = false;
 		String password = "";
 		boolean verifiedPassword = true;
-		boolean exitInput = false;
+		// boolean exitInput = false;
 
 		do {
+			LoggingUtil.trace("LoginVerification(); - do loop - start");
+
 			Scanner scanner = new Scanner(System.in);
 			counter = 0;
 
 			// username validation
 			System.out.println("Enter (b) to return to the initial screen.");
-
 			System.out.println("enter username --> ");
 			username = scanner.nextLine();
 
 			if (username.equals("b")) {
 				break;
 			}
-
 			if (MasterCustomerLoginList.getCustomerloginmap().containsKey(username)) {
 				verifiedUsername = true;
 				counter++;
 			}
-
 			// password validation
 			System.out.println("enter password --> ");
 			password = scanner.nextLine();
@@ -197,71 +123,47 @@ public class CustomerScreen implements UserScreen {
 					counter++;
 				}
 			} else {
-				System.out.println("username not found!");
+				LoggingUtil.debug("LoginVerification(); - do loop - username not found");
+				System.out.println("Username Not Found!");
 			}
-
 			// after failed password attempt
 			if (counter == 1) {
-				System.out.println("wrong password");
+				LoggingUtil.debug("LoginVerification(); - do loop - password not wrong");
+				System.out.println("Wrong Password");
 			}
-
 		} while (!verifiedUsername && !verifiedPassword);
 
-		// will need to ensure the username password have a match
-		// best is probably a master list with MAP
-		// if not register to the map then ask again!
 		if (counter == 2) {
 			// verified let them in!
-			
-			// match the customer to get that customer!
-			//all usernames are unique
-			
-			for(int i = 0; i < CustomerBase.getCustomerlist().size(); i++) {
-				if (CustomerBase.getCustomerlist().get(i).getUsername().equals(username)) {
-					loggedInCustomer = CustomerBase.getCustomerlist().get(i);
-				}
-			}
-			
-			
-			
-			
-			
-			// TODO should track id and return id back
+			LoggingUtil.trace("LoginVerification(); - do loop - login verified");
+			customerLoginMatch(username);
 			return true;
 		} else {
-			// not verified kick them out!
+			LoggingUtil.warn("failed login attempt");
 			return false;
 		}
 	}
 
-	// do loop
-	public boolean register() {
+	public void customerLoginMatch(String username) {
+
+		for (int i = 0; i < CustomerBase.getCustomerlist().size(); i++) {
+			if (CustomerBase.getCustomerlist().get(i).getUsername().equals(username)) {
+				LoggingUtil.debug("customerLoginMatch(); - getting logged in customer");
+				loggedInCustomer = CustomerBase.getCustomerlist().get(i);
+			}
+		}
+	}
+
+	public void register() {
+		LoggingUtil.trace("CustomerScreen - register() - start");
 		System.out.println("-- Customer Registration -- ");
 
 		String username = "";
 		String password = "";
 		boolean exitInput = false;
-		
-		//TODO TODO TODO will need to load here instead!
-		// TODO
-		//TODO
-		// TODO
-		//if(CustomerBase.getCustomerlist().size() != 0) {
-		
-		
-		/*
-		 * if(customerListdata.loadCustomerList() != null &&
-		 * loginListData.loadLoginList() != null) {
-		 * CustomerBase.getCustomerlist().clear();
-		 * CustomerBase.getCustomerlist().addAll(customerListdata.loadCustomerList());
-		 * MasterCustomerLoginList.getCustomerloginmap().putAll(loginListData.
-		 * loadLoginList()); }
-		 */
-		
-
-		
 
 		do {
+			LoggingUtil.trace("CustomerScreen - do loop - start");
 
 			Scanner scanner = new Scanner(System.in);
 
@@ -269,49 +171,26 @@ public class CustomerScreen implements UserScreen {
 			username = scanner.nextLine();
 
 			if (MasterCustomerLoginList.getCustomerloginmap().containsKey(username)) {
-				System.out.println("this username is already taken");
-
+				System.out.println("This Username is Already Taken");
 			} else {
-				System.out.println("enter new password-->");
+				System.out.println("Enter New Password-->");
 				password = scanner.nextLine();
 
-				System.out.println("Success! '" + username + "' is now a registered user!");
+				System.out.println("Success! '" + username + "' is Now a Registered User!");
 
-
-				// list
-				//TODO need to update id
-//				if(CustomerBase.getCustomerlist().size == 0) {
-//					
-//				}
 				CustomerBase.getCustomerlist().add(new Customer(username, password));
-
-				// might b okay, this is just to keep track of all logins!
-				// only accessing this when need to verify username/passwords
-				// match username and password to get customer!
-				// ^
 				MasterCustomerLoginList.getCustomerloginmap().put(username, password);
-				
-				
-				/*
-				 * customerListdata.saveCustomerList(CustomerBase.getCustomerlist());
-				 * loginListData.saveLoginList(MasterCustomerLoginList.getCustomerloginmap());
-				 */
-				
 
 				exitInput = true;
 			}
 
 		} while (!exitInput);
-		
-		//CALL THE METHOD SAVE FILE ON MASTERCUSTOMERLOGINLIST SO IT SAVES THE MASTERCUSTOMERLOGINLIST CLASS
-		// saveLoginList(MasterCustomerLoginList);
-		//should load 
 
-		return true;
 	}
 
-	public boolean customerAccess() {
-
+	// TODO JUNIT TEST
+	public boolean customerAccessMenu() {
+		LoggingUtil.trace("CustomerScreen - customerAccessMenu(); - start");
 		boolean exitInput = false;
 		boolean returnBool = true;
 		String input = "";
@@ -326,27 +205,20 @@ public class CustomerScreen implements UserScreen {
 			input = scanner.nextLine();
 
 			if (input.equals("1")) {
-				// call login
-				
-				
-				//TODO if loginVerification returns a customer than turn exitInput true, & return that customer 
-				//because they are who is logged in. 
-				
-				
+				LoggingUtil.trace("CustomerScreen - customerAccessMenu(); - calling loginVerification()");
 				exitInput = loginVerification();
 			} else if (input.equals("2")) {
-				// call register
+				LoggingUtil.trace("CustomerScreen - customerAccessMenu(); - calling register()");
 				register();
 			} else if (input.equals("0")) {
+				LoggingUtil.trace("CustomerScreen - customerAccessMenu(); - exiting");
 				exitInput = true;
 				returnBool = false;
-
 			}
 
 		} while (!exitInput);
 
 		return returnBool;
-		// call register or loginVerification if not exit
 
 	}
 
